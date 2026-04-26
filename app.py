1658513971c7de5937a026c59880d97b134c9a4a
herman <mmszhezhi@gmail.com>
2026-04-27 04:58:41 +0800

    update

--- app.py
+++ app.py
@@ -650,4 +650,4 @@ def switch_theme(click_list, current_theme):
 
 # ========== 运行应用 ==========
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8050)
+    app.run(debug=True, host='0.0.0.0', port=8055)
